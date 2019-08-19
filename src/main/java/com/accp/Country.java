496b049b2d9412d74f81904d89470bbf96839bbf
merrybiao <787347862@qq.com>
2019-08-19 21:30:41 +0800

    提交到本地V2

---
+++ src/main/java/com/accp/Country.java
@@ -0,0 +1,17 @@
+package com.accp;
+
+public class Country {
+	
+	private static String ggrdString;
+
+	public static String getGgrdString() {
+		return ggrdString;
+	}
+
+	public static void setGgrdString(String ggrdString) {
+		Country.ggrdString = ggrdString;
+	}
+	
+	
+
+}
