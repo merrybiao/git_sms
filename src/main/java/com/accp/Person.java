101fb5a501661eca96fe2e63dec1363087ff4556
merrybiao <41351709+merrybiao@users.noreply.github.com>
2019-08-19 21:39:21 +0800

    github提交V4

--- src/main/java/com/accp/Person.java
+++ src/main/java/com/accp/Person.java
@@ -1,22 +1,6 @@
 package com.accp;
 
 public class Person {
-
-
-	 //发�?�验证码的请求路径URL
-    private static final String
-            SERVER_URL="https://api.netease.im/sms/sendtemplate.action";
-    //网易云信分配的账号，请替换你在管理后台应用下申请的Appkey
-    private static final String
-            APP_KEY="9eb1b7d968095f786d7257701bc12b69";
-    //网易云信分配的密钥，请替换你在管理后台应用下申请的appSecret
-    private static final String APP_SECRET="90493a75775d";
-    //随机�?
-    private static final String NONCE="123456";
-    //短信模板ID
-    private static final String TEMPLATEID="14823014";
-    //手机号，接收者号码列表，JSONArray格式，限制接收�?�号码个数最多为100�?
-    private static final String MOBILES="['15827167589','15827167589']";
   
 	
 	private String name;
